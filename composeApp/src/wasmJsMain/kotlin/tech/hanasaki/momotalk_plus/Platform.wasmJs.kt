777904d87a3be2e0be52eab39a0136cd118da270
hanasaki <hanasakayui2022@gmail.com>
2025-06-25 18:50:23 +0800

    🔥 移除不必要的Platform.kt

--- composeApp/src/wasmJsMain/kotlin/tech/hanasaki/momotalk_plus/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package tech.hanasaki.momotalk_plus
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
